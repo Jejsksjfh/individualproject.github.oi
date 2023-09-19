0e5f371555a1f15608aa12acecc74fa924e4781f
Jejsksjfh <145230166+Jejsksjfh@users.noreply.github.com>
2023-09-19 21:45:23 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.getElementById("toggleContent").addEventListener("click", function() {
-    var hiddenContent = document.getElementById("hiddenContent");
-    if (hiddenContent.style.display === "none") {
-        hiddenContent.style.display = "block";
-    } else {
-        hiddenContent.style.display = "none";
-    }
-});
